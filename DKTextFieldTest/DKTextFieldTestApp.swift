1e1146304117aa5727be0dd16e131c55deb95a70
Jakob Schøler <110392921+jscholer-foreflight@users.noreply.github.com>
2024-09-24 12:06:43 +0200

    Initial Commit

---
+++ DKTextFieldTest/DKTextFieldTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DKTextFieldTestApp.swift
+//  DKTextFieldTest
+//
+//  Created by Jakob Schøler on 24/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct DKTextFieldTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
